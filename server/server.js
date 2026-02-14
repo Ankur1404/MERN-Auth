625fb319c12e8edb172ba9169586c8d8d5bb0492
Ankur Kumar <ankurkumarmr@gmail.com>
2026-02-15 01:05:56 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -7,9 +7,15 @@ import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
 const PORT = process.env.PORT || 4000;
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins =process.env.NODE_ENV === "production"
+  ? [process.env.PRODUCTION_FRONTEND_URL]
+  : [process.env.FRONTEND_URL];
 app.use(express.json());
 app.use(cookieParser());
 app.use(
