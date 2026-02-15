1ab553eda6cd56cbfa2677c1cfeefbf9811157cd
Ankur Kumar <ankurkumarmr@gmail.com>
2026-02-15 16:17:57 +0530

    port update for email

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,14 +1,20 @@
 import nodemailer from 'nodemailer';
+import dns from 'dns';
+
+// Force Node to prefer IPv4 over IPv6
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-  
-    port: 587,
-  secure: false, 
-    auth:{
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // must be false for 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 transporter.verify((error, success) => {
