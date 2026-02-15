fb448420f316e2bb335e6e61d7403dec1b69b3e1
Ankur Kumar <ankurkumarmr@gmail.com>
2026-02-15 22:36:00 +0530

    changes

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,7 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+   host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    logger: true,
+    debug: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
