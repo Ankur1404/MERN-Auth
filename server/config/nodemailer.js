3911a1312d240d37424b350e552e8b0aa4d741c8
Ankur Kumar <ankurkumarmr@gmail.com>
2026-02-15 16:34:29 +0530

    simpler config

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,27 +1,18 @@
-import nodemailer from 'nodemailer';
-import dns from 'dns';
-
-// Force Node to prefer IPv4 over IPv6
-dns.setDefaultResultOrder("ipv4first");
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // must be false for 587
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    pass: process.env.SMTP_PASS,
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
 
 transporter.verify((error, success) => {
   if (error) {
-    console.log('Email transporter error:', error);
+    console.log("Email transporter error:", error);
   } else {
-    console.log('Email transporter is ready');
+    console.log("Email transporter is ready");
   }
 });
 
